ce7ac1eaa904e70e70b9700798349ec0670738ad
David Hou <david.m.hou@gmail.com>
2019-11-02 02:16:19 +0000

    convert tab to spaces

--- website/app/routes.py
+++ website/app/routes.py
@@ -20,7 +20,7 @@ def allowed_file(filename):
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/images')
 def home():
@@ -87,26 +87,26 @@ def example_parrot():
 
 @app.route('/models')
 def models():
-	return render_template('models.html', main_title='ML Models')
+    return render_template('models.html', main_title='ML Models')
 
 @app.route('/posts')
 def posts():
-	return render_template('posts.html', main_title='Posts/Comments')
+    return render_template('posts.html', main_title='Posts/Comments')
 
 @app.route('/subreddits', methods=['GET', 'POST'])
 def subreddits():
-	return render_template(
-		'subreddits.html',
-		main_title='Subreddits/Users')
+    return render_template(
+        'subreddits.html',
+        main_title='Subreddits/Users')
 
 @app.route('/closest_subreddits/<subreddit>', methods=['GET'])
 def closest_subreddits(subreddit):
-	response = [
-		{'subreddit': x[0], 'distance': x[1]} for x in subreddit_embeddings.get_closest(subreddit)
-	]
-	return jsonify(response)
+    response = [
+        {'subreddit': x[0], 'distance': x[1]} for x in subreddit_embeddings.get_closest(subreddit)
+    ]
+    return jsonify(response)
 
 
 @app.route('/post_counts/<subreddit>', methods=['GET'])
 def get_post_counts(subreddit):
-	return jsonify(post_counts.get_counts(subreddit))
+    return jsonify(post_counts.get_counts(subreddit))
